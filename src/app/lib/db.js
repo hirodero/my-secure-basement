efb66c9422828f6cde117b78072dfc0d1ac36181
hirodero <idexuta@gmail.com>
2025-05-30 14:33:16 +0700

    env configuration change

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -2,11 +2,13 @@ import mysql from 'mysql2/promise'
 
 export async function query(sql, values = []) {
   const config = {
-    user: process.env.DATABASE_USER,
-    host: process.env.DATABASE_HOST,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME
-  }
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT 
+}
+
   const connection = await mysql.createConnection(config)
   try {
     const [rows] = await connection.execute(sql, values)
